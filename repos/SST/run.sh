76fcb9d0fb003fbb23727939f5c38c0d77a2b87e
Adrian Chow <ahtchow@gmail.com>
2023-10-08 17:34:15 -0400

    Update SST for 5f+ expirements

--- repos/SST/run.sh
+++ repos/SST/run.sh
@@ -1,6 +1,6 @@
 DIR=fsd
 WORK=work_dirs
-CONFIG=fsd_waymoD1_1x
+CONFIG=fsd_waymoD1_1x_5f_6e
 bash tools/dist_train.sh configs/$DIR/$CONFIG.py 4 --work-dir ./$WORK/$CONFIG/ --cfg-options evaluation.pklfile_prefix=./$WORK/$CONFIG/results evaluation.metric=fast --seed 1
 
 # CTRL training
@@ -9,4 +9,4 @@ bash tools/dist_train.sh configs/$DIR/$CONFIG.py 4 --work-dir ./$WORK/$CONFIG/ -
 # bash tools/dist_train.sh configs/$DIR/$CONFIG.py 8 --work-dir ./$WORK/$CONFIG/ --no-validate
 
 # fast Waymo Evaluation, for all waymo-based models
-# bash tools/dist_test.sh configs/$DIR/$CONFIG.py ./$WORK/$CONFIG/latest.pth 8 --options "pklfile_prefix=./$WORK/$CONFIG/results" --eval fast
+# bash tools/dist_test.sh configs/$DIR/$CONFIG.py ./$WORK/$CONFIG/latest.pth 4 --options "pklfile_prefix=./$WORK/$CONFIG/results" --eval fast
--- repos/SST/setup_waymo.sh
+++ repos/SST/setup_waymo.sh
@@ -2,6 +2,7 @@
 
 HOME="/work_dir"
 SST_PATH="${HOME}/SST"
+WAYMO_OD_PATH="${HOME}/waymo-od"
 
 # Symbolically Link Waymo Dataset
 WAYMO_DATA_PATH="${SST_PATH}/data/waymo"
@@ -11,3 +12,13 @@ else
     mkdir -p data/waymo
     ln -s ${HOME}/datasets/waymo/mmdet3d_format/kitti_format ${WAYMO_DATA_PATH}
 fi
+
+# Update Compute Metrics File
+cd ${WAYMO_OD_PATH}/src
+HOME=${WAYMO_OD_PATH} bazel clean
+HOME=${WAYMO_OD_PATH} bazel build waymo_open_dataset/metrics/tools/compute_detection_metrics_main
+if [ -f ${SST_PATH}/mmdet3d/core/evaluation/waymo_utils/compute_detection_metrics_main ]; then
+    chmod +w ${SST_PATH}/mmdet3d/core/evaluation/waymo_utils/compute_detection_metrics_main
+fi
+cp bazel-bin/waymo_open_dataset/metrics/tools/compute_detection_metrics_main ${SST_PATH}/mmdet3d/core/evaluation/waymo_utils/
+cd ${SST_PATH}
